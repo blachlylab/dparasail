57ed001f07c1f7803063e7c6f1e8a577d42f46b0
cgregory <charles.gregory940@topper.wku.edu>
2018-11-26 14:25:38 -0500

    fixed linking errors and fixed more bugs

--- source/app.d
+++ source/app.d
@@ -1,10 +1,43 @@
 import std.stdio;
+import std.array:array;
 import dparasail;
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
 	auto p=Parasail("dnafull",3,2);
-	p.sw_striped("GGGATCA","GACTAAA");
-	writeln(p.result.score);
-	writeln(p.get_cigar());
+	auto q=p.sw_striped("GATTA","GACTA");
+	writeln(q.seq1[0..q.seq1Len]);
+	writeln(q.get_cigar(p.score_matrix));
+	writeln(q.beg_query);
+	writeln(q.beg_ref);
+	//assert(q.cigar=="2=1X2=");
+	q=p.sw_striped("GGCTTCTGATCAGGCTTCT","GACTA");
+	writeln(q.seq1[0..q.seq1Len]);
+	writeln(q.get_cigar(p.score_matrix));
+	writeln(q.beg_query);
+	writeln(q.beg_ref);
+	//assert(q.cigar=="7S2=1D1=1I1=7S");
+	q=p.sw_striped("GACTAA","GGCTTCTGATCAGGCTTCT");
+	writeln(q.seq1[0..q.seq1Len]);
+	writeln(q.get_cigar(p.score_matrix));
+	writeln(q.beg_query);
+	writeln(q.beg_ref);
+	q=p.nw_scan("GATTA","GACTA");
+	writeln(q.seq1[0..q.seq1Len]);
+	writeln(q.get_cigar(p.score_matrix));
+	writeln(q.beg_query);
+	writeln(q.beg_ref);
+	//assert(q.cigar=="2=1X2=");
+	q=p.nw_scan("GGCTTCTGATCAGGCTTCT","GACTA");
+	writeln(q.seq1[0..q.seq1Len]);
+	writeln(q.get_cigar(p.score_matrix));
+	writeln(q.beg_query);
+	writeln(q.beg_ref);
+	//assert(q.cigar=="1=1X2=4I1=10S");
+	q=p.nw_scan("GACTAA","GGCTTCTGATCAGGCTTCT");
+	writeln(q.seq1[0..q.seq1Len]);
+	writeln(q.get_cigar(p.score_matrix));
+	writeln(q.beg_query);
+	writeln(q.beg_ref);
+	auto cigar=parasail_result_get_cigar(q.result,q.seq1,q.seq1Len,q.seq2,q.seq2Len,p.score_matrix);
+	writeln(parasail_cigar_decode(cigar));
 }
--- source/dparasail.d
+++ source/dparasail.d
@@ -2,6 +2,9 @@ module dparasail;
 import std.stdio;
 import std.conv;
 import std.utf;
+import bio.bam.cigar;
+import std.algorithm:map,filter;
+import std.algorithm.iteration:sum;
 extern(C):
 
 //Struct declarations
@@ -26,12 +29,6 @@ parasail_matrix_t* parasail_matrix_create(
 void parasail_matrix_free(parasail_matrix_t* matrix);
 
 //SW functions
-parasail_cigar_t* parasail_result_get_cigar(
-        parasail_result_t *result,
-        const char *seqA, int lena,
-        const char *seqB, int lenb,
-        const parasail_matrix_t *matrix);
-
 
 parasail_result_t* parasail_sw_trace_striped_16(
         const char * s1,const int s1Len,
@@ -54,6 +51,12 @@ void parasail_cigar_free(parasail_cigar_t *cigar);
 char parasail_cigar_decode_op(uint cigar_int);
 uint parasail_cigar_decode_len(uint cigar_int);
 char* parasail_cigar_decode(parasail_cigar_t *cigar);
+parasail_cigar_t* parasail_result_get_cigar(
+        parasail_result_t *result,
+        const char *seqA, int lena,
+        const char *seqB, int lenb,
+        const parasail_matrix_t *matrix);
+
 
 
 //D wrapping
@@ -71,63 +74,103 @@ struct parasail_query{
     int seq2Len;
     int beg_query;
     int beg_ref;
-    string cigar;
     parasail_result_t* result;
-    void get_cigar(parasail_matrix_t* score_matrix){
+    //string get_cigar(parasail_matrix_t* score_matrix){
+    //    parasail_cigar_t* cigar;
+    //    string cigar_string;
+    //    int i=0;
+    //    cigar=parasail_result_get_cigar(result,seq1,seq1Len,seq2,seq2Len,score_matrix);
+    //    beg_query=cigar.beg_query;
+    //    beg_ref=cigar.beg_ref;
+    //    char letter;
+    //    uint length;
+    //    int count=0;
+    //    if(this.beg_query!=0){
+    //        length=this.beg_query;
+    //        letter=parasail_cigar_decode_op(cigar.seq[0]);
+    //        if(letter=='I'){
+    //            length+= parasail_cigar_decode_len(cigar.seq[0]);
+    //            i=1;
+    //        }else if(letter=='*'){
+    //            this.cigar="*";
+    //            return;
+    //        }
+    //        count+=length;
+    //        letter='S';
+    //        cigar_string=cigar_string~to!string(length)~letter;
+    //    }
+    //    for (;i<cigar.len; ++i) {
+    //        letter=parasail_cigar_decode_op(cigar.seq[i]);
+    //        length= parasail_cigar_decode_len(cigar.seq[i]);
+    //        if(i==0 && letter=='I'){
+    //            letter='S';
+    //        }
+    //        if(i==0 && letter=='D'){
+    //            letter='S';
+    //            this.beg_ref+=length;
+    //            continue;
+    //        }
+    //        if(i==(cigar.len-1) && letter=='I'){
+    //            letter='S';
+    //        }
+    //        if(letter!='D'){
+    //            count+=length;
+    //        }
+    //        //if((i==0 && letter=='D')|(i==(cigar.len-1) && letter=='D')){
+    //        //    continue;
+    //        //}
+    //        cigar_string=cigar_string~to!string(length)~letter;
+    //    }
+    //    letter='S';
+    //    length=seq1Len-result.end_query-1;
+    //    if (length>0){
+    //        count+=length;
+    //        cigar_string=cigar_string~to!string(length)~letter;
+    //    }
+    //    assert(count==seq1Len);
+    //    parasail_cigar_free(cigar);
+    //    return cigar_string;
+    //}
+    CigarOperations get_cigar(parasail_matrix_t* score_matrix){
         parasail_cigar_t* cigar;
-        string cigar_string;
-        int i=0;
+        CigarOperations cigar_string;
         cigar=parasail_result_get_cigar(result,seq1,seq1Len,seq2,seq2Len,score_matrix);
         beg_query=cigar.beg_query;
         beg_ref=cigar.beg_ref;
-        char letter;
-        uint length;
-        int count=0;
-        if(this.beg_query!=0){
-            length=this.beg_query;
-            letter=parasail_cigar_decode_op(cigar.seq[0]);
-            if(letter=='I'){
-                length+= parasail_cigar_decode_len(cigar.seq[0]);
-                i=1;
-            }else if(letter=='*'){
-                this.cigar="*";
-                return;
-            }
-            count+=length;
-            letter='S';
-            cigar_string=cigar_string~to!string(length)~letter;
+        for (auto i=0;i<cigar.len; ++i) {
+            cigar_string~=CigarOperation(parasail_cigar_decode_len(cigar.seq[i]),parasail_cigar_decode_op(cigar.seq[i]));
         }
-        for (;i<cigar.len; ++i) {
-            letter=parasail_cigar_decode_op(cigar.seq[i]);
-            length= parasail_cigar_decode_len(cigar.seq[i]);
-            if(i==0 && letter=='I'){
-                letter='S';
-            }
-            if(i==0 && letter=='D'){
-                letter='S';
-                this.beg_ref+=length;
-                continue;
-            }
-            if(i==(cigar.len-1) && letter=='I'){
-                letter='S';
-            }
-            if(letter!='D'){
-                count+=length;
-            }
-            //if((i==0 && letter=='D')|(i==(cigar.len-1) && letter=='D')){
-            //    continue;
-            //}
-            cigar_string=cigar_string~to!string(length)~letter;
+        if(cigar_string.is_unavailable){
+            return cigar_string;
         }
-        letter='S';
-        length=seq1Len-result.end_query-1;
-        if (length>0){
-            count+=length;
-            cigar_string=cigar_string~to!string(length)~letter;
+        if(cigar_string[0].type=='I'){
+            cigar_string[0]=CigarOperation(cigar_string[0].length+this.beg_query,'S');
+            this.beg_query=0;
         }
-        assert(count==seq1Len);
+        else if(cigar_string[0].type=='D'){
+            this.beg_ref=this.beg_ref+cigar_string[0].length;
+            //cigar_string[0]=CigarOperation(cigar_string[0].length+this.beg_query,'S');
+        }
+        else if(this.beg_query!=0){
+            cigar_string=CigarOperation(this.beg_query,'S')~cigar_string;
+            this.beg_query=0;
+        }
+        ///////////////////////////////////////////////////////////
+        int q_bases_covered=cast(int) cigar_string.filter!(x=>x.is_query_consuming()).map!(x=>x.length).sum;
+        if(cigar_string[$-1].type=='I'){
+            cigar_string[$-1]=CigarOperation(cigar_string[$-1].length+this.seq1Len-q_bases_covered,'S');
+            q_bases_covered=cigar_string.filter!(x=>x.is_query_consuming()).map!(x=>x.length).sum;
+        }
+        else if(cigar_string[$-1].type=='D'){
+            cigar_string=cigar_string[0..($-1)];
+        }
+        else if(q_bases_covered!=this.seq1Len){
+            cigar_string~=CigarOperation(this.seq1Len-q_bases_covered,'S');
+            q_bases_covered=cigar_string.filter!(x=>x.is_query_consuming()).map!(x=>x.length).sum;
+        }
+        assert(q_bases_covered==seq1Len);
         parasail_cigar_free(cigar);
-        this.cigar=cigar_string;
+        return cigar_string;
     }
     void close(){
         parasail_result_free(result);
@@ -158,7 +201,7 @@ struct Parasail{
         p.seq1Len=s1Len;
         p.seq2Len=s2Len;
         p.result=sw_striped(p);
-        p.get_cigar(this.score_matrix);
+        //p.cigar=p.get_cigar(this.score_matrix);
         return p;
     }
     parasail_result_t* sw_striped(parasail_query p){
@@ -174,7 +217,6 @@ struct Parasail{
         p.seq1Len=s1Len;
         p.seq2Len=s2Len;
         p.result=nw_scan(p);
-        p.get_cigar(this.score_matrix);
         return p;
     }
     parasail_result_t* nw_scan(parasail_query p){
@@ -222,5 +264,7 @@ unittest{
     writeln(q.cigar);
     writeln(q.beg_query);
     writeln(q.beg_ref);
+    auto cigar=parasail_result_get_cigar(q.result,q.seq1,q.seq1Len,q.seq2,q.seq2Len,q.score_matrix);
+    writeln(parasail_cigar_decode(cigar));
 }
 
